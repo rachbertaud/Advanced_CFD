bce588b684454883cabf6f064efb40ff5c40caa9
Rachel <rybdancer@gmail.com>
2026-01-29 16:52:42 -0700

    My first C++ code

---
+++ HW1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char *argv[])
+{
+
+	double N = atof(argv[1]);
+	std::cout << "N =" << N << std::endl;
+
+
+}
---
+++ Makefile
@@ -0,0 +1,7 @@
+build_HW1: 
+	g++ -o run_HW1 HW1.cpp
+
+HW1:
+	./run_HW1 10
+	./run_HW1 50
+	./run_HW1 100 
