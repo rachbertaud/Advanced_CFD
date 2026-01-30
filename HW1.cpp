078c2ce827b11e9dfae1ccc514e00d4f4d0a629b
Rachel <rybdancer@gmail.com>
2026-01-29 18:22:39 -0700

    Finsihed question 5. Included error metric and print statements

--- HW1.cpp
+++ HW1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
+
+//defines namespace as std so we don't have to repeat it
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -12,7 +15,7 @@ int main(int argc, char *argv[])
 	const int N_int = atoi(argv[1]);
 	
 	//print out result of N to confirm we are getting the N we expect
-	cout << "N =" << N << endl;
+	cout << "N = " << N << endl;
 	
 	//define pi as an unchangeable constant
 	const double pi = 4.0*atan(1.0); //this line was referenced from CPP Companion
@@ -22,7 +25,7 @@ int main(int argc, char *argv[])
 
 	//define h as N/L
 	double h = L/N;
-	cout << "Checking L/N = " << L << "/" << N << " = " << h << " = h" << endl;
+	//cout << "Checking L/N = " << L << "/" << N << " = " << h << " = h" << endl;
 
 	//------------------------------- Question 2 ----------------------------------------
 
@@ -67,7 +70,36 @@ int main(int argc, char *argv[])
 	
 	//------------------------------- Question 4 ----------------------------------------
 	
-	double diff_f_sol = 
+	//declare a difference matrix for error calculation
+	double diff_f_sol[N_int]; 
+	
+	//define starting max values, can use 0 as we are taking fabs	
+	double max_diff = 0;
+	double max_analytical = 0;
+
+	for(int i = 0; i < N; i++){
+		//calculates the floating point absolute difference between our analytical and numerical solution
+		diff_f_sol[i] = fabs(f_analytical[i] - f_numerical[i]);
+
+		//checks to see if the current i-th entry of diff_f_sol is the maximum
+		if(fabs(diff_f_sol[i] - max_diff) > 1e-5){
+			max_diff = diff_f_sol[i];
+		}
+
+		//determines maximum value of f_analytical
+		if(fabs(f_analytical[i] - max_analytical) > 1e-5){
+			max_analytical = fabs(f_analytical[i]);
+		}
+
+
+	}
+
+	double E_inf = max_diff/max_analytical;
+	cout << "The global error, E_inf, is " << E_inf << endl;
+	
+		
+	
+	
 	
 	
 	cout << "---------------------------" << endl;
--- Makefile
+++ Makefile
@@ -2,9 +2,11 @@ build_HW1: HW1.cpp
 	g++ -o run_HW1 HW1.cpp
 
 HW1: build_HW1 #info after : declares dependancies
-	./run_HW1 10
-	./run_HW1 50
-	./run_HW1 100
+	./run_HW1 16
+	./run_HW1 32
+	./run_HW1 64
+	./run_HW1 128
+	./run_HW1 256	
 
 clean: 
 	rm -f run_HW1 
