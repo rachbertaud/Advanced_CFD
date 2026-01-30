0e124c721506b4c681b4e6fe51afb8b720673355
Rachel <rybdancer@gmail.com>
2026-01-30 15:36:55 -0700

    Clean up and ready for submission

--- HW1.cpp
+++ HW1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <fstream> //for data writing purposes
 
 //defines namespace as std so we don't have to repeat it
 using namespace std;
@@ -9,92 +10,109 @@ int main(int argc, char *argv[])
 	//------------------------------- Question 1 ----------------------------------------
 	cout << "---------------------------" << endl;
 	//define N using user-input in float form and in as an int > this might not be necessary and should be changed later but lets see what happens 
- 
-	const int N = atoi(argv[1]);
+ 	
+	//user input for how many times they want to run the process (for how many N)
+	const int stop = atoi(argv[1]);
 	
-	//print out result of N to confirm we are getting the N we expect
-	cout << "N = " << N << endl;
-	
-	//define pi as an unchangeable constant
-	const double pi = 4.0*atan(1.0); //this line was referenced from CPP Companion
-	
-	//define L as 2*pi
-	double L = 2*pi;
+	//all file writing info referenced from https://stackoverflow.com/questions/25201131/writing-csv-files-from-c
+	ofstream file;
+	file.open ("HW1_Error_Data.csv"); //opens/makes data file
+	file << "N, Err_inf, \n"; //writes header	
 
-	//define h as N/L
-	double h = L/static_cast<double>(N);
-	//cout << "Checking L/N = " << L << "/" << N << " = " << h << " = h" << endl;
 
-	//------------------------------- Question 2 ----------------------------------------
+	//this for loops runs through the main code for different values of N, all multiples of 16
+	for(int N = 16; N < (16*2*(stop -1) + 1); N *= 2){	
+		//print out result of N to confirm we are getting the N we expect
+		cout << "N = " << N << endl;
+		
+		//define pi as an unchangeable constant
+		const double pi = 4.0*atan(1.0); //this line was referenced from CPP Companion
+		
+		//define L as 2*pi
+		double L = 2*pi;
 
-	
-	//define grid points
-	double x[N];
+		//define h as N/L
+		double h = L/static_cast<double>(N);
+		//cout << "Checking L/N = " << L << "/" << N << " = " << h << " = h" << endl;
 
-	//define function points
-	double f[N];
+//------------------------------- Question 2 ----------------------------------------
 
-	//define numerical derivative array 
-	double f_numerical[N];
+		
+		//define grid points
+		double x[N];
 
-	//define analytical derivative 
-	double f_analytical[N];
-	
-	//define 2pi/L so I don't have to write it 13 times
-	double C = (2*pi)/L; 
+		//define function points
+		double f[N];
 
-	for(int i=0; i < N; i++){
-		x[i] = i*h; //declares the grid point by using h
-		f[i] = exp(sin(C*x[i])); //declares function we wish to evaluate the derivative of
-		f_analytical[i] = C*cos(C*x[i])*exp(sin(C*x[i])); //declares teh analytical solution to f
+		//define numerical derivative array 
+		double f_numerical[N];
+
+		//define analytical derivative 
+		double f_analytical[N];
+		
+		//define 2pi/L so I don't have to write it 13 times
+		double C = (2*pi)/L; 
+
+		for(int i=0; i < N; i++){
+			x[i] = i*h; //declares the grid point by using h
+			f[i] = exp(sin(C*x[i])); //declares function we wish to evaluate the derivative of
+			f_analytical[i] = C*cos(C*x[i])*exp(sin(C*x[i])); //declares teh analytical solution to f
+		}
+
+//------------------------------- Question 3 ----------------------------------------
+
+		for(int i = 0; i < N; i ++){
+			if(i == 0){
+				//if I'm on the left boundary, I want the last input of f
+				f_numerical[i] = (f[i + 1] - f[N - 1])/(2*h);
+			}
+			else if (i == (N - 1)){
+				//if I'm at my right boundary, I want my first input of f
+				f_numerical[i] = (f[0] - f[i - 1])/(2*h);
+			}
+			else{
+				//else,I just want my neighbors
+				f_numerical[i] = (f[i + 1] - f[i - 1])/(2*h);
+			}
+		}
+		
+//------------------------------- Question 4 ----------------------------------------
+		
+		//declare a difference matrix for error calculation
+		double diff_f_sol[N]; 
+		
+		//define starting max values, can use 0 as we are taking fabs	
+		double max_diff = 0;
+		double max_analytical = 0;
+
+		for(int i = 0; i < N; i++){
+			//calculates the floating point absolute difference between our analytical and numerical solution
+			diff_f_sol[i] = fabs(f_analytical[i] - f_numerical[i]);
+
+			//checks to see if the current i-th entry of diff_f_sol is the maximum
+			if(diff_f_sol[i] > max_diff){
+				max_diff = diff_f_sol[i];
+			}
+
+			//determines maximum value of f_analytical
+			if(fabs(f_analytical[i]) >  max_analytical){
+				max_analytical = fabs(f_analytical[i]);
+			}
+
+
+		}
+		
+		double E_inf = max_diff/max_analytical;
+		cout << "The global error, E_inf, is " << E_inf << endl;
+		
+		cout << "---------------------------" << endl;
+		
+		//writes current data to csv file
+		file << N << "," << E_inf << ",\n";
 	}
-
-	//------------------------------- Question 3 ----------------------------------------
-
-	for(int i = 0; i < N; i ++){
-		if(i == 0){
-			//if I'm on the left boundary, I want the last input of f
-			f_numerical[i] = (f[i + 1] - f[N - 1])/(2*h);
-		}
-		else if (i == (N - 1)){
-			//if I'm at my right boundary, I want my first input of f
-			f_numerical[i] = (f[0] - f[i - 1])/(2*h);
-		}
-		else{
-			//else,I just want my neighbors
-			f_numerical[i] = (f[i + 1] - f[i - 1])/(2*h);
-		}
-	}
 	
-	//------------------------------- Question 4 ----------------------------------------
+	file.close(); //closes csv file
+	return 0;
 	
-	//declare a difference matrix for error calculation
-	double diff_f_sol[N]; 
-	
-	//define starting max values, can use 0 as we are taking fabs	
-	double max_diff = 0;
-	double max_analytical = 0;
-
-	for(int i = 0; i < N; i++){
-		//calculates the floating point absolute difference between our analytical and numerical solution
-		diff_f_sol[i] = fabs(f_analytical[i] - f_numerical[i]);
-
-		//checks to see if the current i-th entry of diff_f_sol is the maximum
-		if(diff_f_sol[i] > max_diff){
-			max_diff = diff_f_sol[i];
-		}
-
-		//determines maximum value of f_analytical
-		if(fabs(f_analytical[i]) >  max_analytical){
-			max_analytical = fabs(f_analytical[i]);
-		}
-
-
-	}
-	
-	double E_inf = max_diff/max_analytical;
-	cout << "The global error, E_inf, is " << E_inf << endl;
-	
-	cout << "---------------------------" << endl;
 //this ends main
 }
--- Makefile
+++ Makefile
@@ -1,12 +1,7 @@
-build_HW1: HW1.cpp 
+HW1: HW1.cpp 
 	g++ -o run_HW1 HW1.cpp
-
-HW1: build_HW1 #info after : declares dependancies
-	./run_HW1 16
-	./run_HW1 32
-	./run_HW1 64
-	./run_HW1 128
-	./run_HW1 256	
+	./run_HW1 10 #user input how many N to investigate
 
 clean: 
-	rm -f run_HW1 
+	rm -f run_HW1
+	rm -f HW1_Error_Plot.csv 
