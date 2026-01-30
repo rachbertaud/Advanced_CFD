6f41964132149b5de2d754ac8554450629cc2a92
Rachel <rybdancer@gmail.com>
2026-01-29 17:20:29 -0700

    Question 1 done

--- HW1.cpp
+++ HW1.cpp
@@ -3,9 +3,34 @@
 
 int main(int argc, char *argv[])
 {
+	//------------------------------- Question 1 ----------------------------------------
 
+	//define N using user-input in float form and in as an int > this might not be necessary and should be changed later but lets see what happens 
+ 
 	double N = atof(argv[1]);
+	const int N_int = atoi(argv[1]);
+	
+	//print out result of N to confirm we are getting the N we expect
 	std::cout << "N =" << N << std::endl;
+	
+	//define pi as an unchangeable constant
+	const double pi = 4.0*std::atan(1.0); //this line was referenced from CPP Companion
+	
+	//define L as 2*pi
+	double L = 2*pi;
+
+	//define h as N/L
+	double h = L/N;
+	std::cout << "Checking L/N = " << L << "/" << N << " = " << h << " = h" << std::endl;
+
+	//------------------------------- Question 2 ----------------------------------------
+
+	
+	//define grid points
+	double x[N_int]
+
+	//define function points
+	double f[N]
 
 
 }
--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-build_HW1: 
+build_HW1: HW1.cpp 
 	g++ -o run_HW1 HW1.cpp
 
-HW1:
+HW1: build_HW1 #info after : declares dependancies
 	./run_HW1 10
 	./run_HW1 50
-	./run_HW1 100 
+	./run_HW1 100
+
+clean: 
+	rm -f run_HW1 
