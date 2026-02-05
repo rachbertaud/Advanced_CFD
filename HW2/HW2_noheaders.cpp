079e378e65efa37a7e4392d49070045131eb9b6e
Rachel <rybdancer@gmail.com>
2026-02-05 15:10:32 -0700

    Made file with no headers to share

--- HW2/HW2_noheaders.cpp
+++ HW2/HW2_noheaders.cpp
@@ -6,6 +6,38 @@
 #include "HW2.h"
 
 
+void read_inputs(std::map<std::string, double>& variables)
+{
+	//define a string 
+	std::string line;
+	std::string var_name;
+
+	//open the input file 
+	std::ifstream inputfile;
+	inputfile.open("inputs");
+	
+
+	double value;
+	int i = 0;
+	
+	while(std::getline(inputfile, line))
+	{
+		if(line=="endfile") break;
+		
+		if(i % 2 == 0){
+			std::stringstream(line) >> var_name;
+		}
+		else{
+			std::stringstream(line) >> value;
+			variables[var_name] = value;
+		}
+		
+		i++;
+	}
+
+	inputfile.close();
+}
+
 int main()
 {
 	std::map<std::string, double> variables;
