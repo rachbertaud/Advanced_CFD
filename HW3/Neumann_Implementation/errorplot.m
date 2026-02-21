d4473976e7ac06a1df8ae22660bad0e459c971cb
Rachel <rybdancer@gmail.com>
2026-02-20 21:36:05 -0700

    Added Neumann BC

---
+++ HW3/Neumann_Implementation/errorplot.m
@@ -0,0 +1,57 @@
+addpath /Users/rachelbertaud/code/MATLAB_Settings/;
+
+plot_settings(1);
+
+%%
+
+Err_inf = [];
+
+for N = [64 128 256 512]
+    x = readmatrix(sprintf('xc_N%d.dat', N));
+    y = readmatrix(sprintf('yc_N%d.dat', N));
+    c = readmatrix(sprintf('c_N%d.dat', N));
+
+    [X,Y] = meshgrid(x,y);
+
+    Nx = length(x);
+    Ny = length(y);
+
+    c_num = reshape(c, Ny, Nx);
+    
+    c_an = zeros(Ny, Nx);
+
+    for i = 2:1:Nx-1
+        for j = 2:1:Ny-1
+            row = (Nx)*(j - 1) + i;
+            c_an(j,i) = cos(x(i))*cos(y(j));
+        end
+    end 
+
+
+
+    c_num_NB = c_num(2:Ny - 1, 2:Nx - 1);
+    c_an_NB = c_an(2:Ny - 1, 2:Nx - 1);
+    X_NB = X(2:Ny - 1, 2:Nx - 1);
+    Y_NB = Y(2:Ny - 1, 2:Nx - 1);
+
+    err = max(max(abs(c_num_NB - c_an_NB)))/max(max(abs(c_an_NB)));
+    Err_inf = [Err_inf err];
+
+end
+
+N = [64 128 256 512];
+
+
+f = figure(1);
+theme(f,"light");
+hold on
+plot(N, (Err_inf), 'Color',[227/255,159/255,246/255])
+plot(N, 1./N, '.-.', 'Color',[240/255,159/255,200/255])
+plot(N, 1./(N.^2), '--', 'Color',[220/255,200/255,246/255])
+set(gca,'YScale','log','XScale','log')
+legend('Results from HW3 Solution', '$\cal{O}$$(h)$ Reference Line', '$\cal{O}$$(h^2)$ Reference Line', 'Location','northeast')
+ylabel("Relative Error of Solution, $E_{\infty}$")
+xlabel("Number of Grid Points, $N$")
+hold off
+
+saveas(f, "HW3_Error_Plot_Neumann.png", 'png')
---
+++ HW3/Neumann_Implementation/funcs_Build.cpp
@@ -0,0 +1,183 @@
+#include <iostream> 		//for cout
+#include <vector>
+#include <cmath>
+
+#include "Header_Build.h"	// for builder funcs/structs
+#include "Header_Mesh.h"	// for mesh funcs/structs
+#include "Header_Case.h"	// for case funcs/structs
+
+void matrixType::init(const meshType& Mesh)
+{
+	Nx = Mesh.Nx;
+	Ny = Mesh.Ny;
+	Nx2 = Nx + 2;
+	Ny2 = Ny + 2;
+	B.assign(Nx2*Ny2, 0.0);
+}
+
+void matrixType::addEntry(int row, int col, double val)
+{
+	ROW.push_back(row);
+	COL.push_back(col);
+	VAL.push_back(val);
+}
+
+void matrixType::setRHS(int i, int j, double val)
+{
+	B[i + j*Nx2] = val;
+}
+
+double matrixType::getRHS(int i, int j) const
+{
+	return B[i + j*Nx2];
+}
+
+void BuildA(matrixType& Matrix, 
+	    const meshType& Mesh, 
+	    const caseType& Case)
+{
+	int Nx = Matrix.Nx;
+	int Ny = Matrix.Ny;
+	int Nx2 = Matrix.Nx2;
+	int Ny2 = Matrix.Ny2;
+	double dx = Mesh.dx;
+	double dy = Mesh.dy;
+	double k = Case.k;
+
+	int row;
+
+	//south/north boundary conditions (Dirichlet)
+	for (int i = 1; i <= Nx; i++)
+	{
+		// south 
+		row = i;
+		Matrix.addEntry(row, row , 0.5);
+		Matrix.addEntry(row, row + Nx2, 0.5);
+
+		//north 
+		row = i + (Nx2*(Ny + 1));
+		Matrix.addEntry(row, row, 0.5);
+		Matrix.addEntry(row, row - Nx2, 0.5);
+	}
+
+	//east/west boundary conditions (Neumann)
+	for (int j = 1; j <= Ny; j++)
+	{
+		//west
+		row = Nx2*j;
+		Matrix.addEntry(row, row, -1/dx);
+		Matrix.addEntry(row, row + 1, 1/dx);
+
+		//east 
+		row = Nx2*(j + 1) - 1;
+		Matrix.addEntry(row, row, 1/dx);
+		Matrix.addEntry(row, row - 1, -1/dx);
+	}
+
+	//five-point stencil
+	double De  = k*dy/dx;
+	double Dw = k*dy/dx;
+	double Ds = k*dx/dy;
+	double Dn = k*dx/dy;
+	double Dp = De + Dw + Ds + Dn;
+
+	double Ae = 0;
+	double Aw = 0;
+	double As = 0;
+	double An = 0;
+	double Ap = 0;
+
+	double ae = De + Ae;
+	double aw = Dw + Aw;
+	double as = Ds + As;
+	double an = Dn + An;
+	double ap = Dp + Ap;
+
+	//internal nodes of A
+	
+	for (int i = 1; i <= Nx; i++)
+	{
+		for (int j = 1; j <= Ny; j++)
+		{
+			row = Nx2*j + i;
+	
+			Matrix.addEntry(row, row, ap);
+			Matrix.addEntry(row, row + 1, -ae);
+			Matrix.addEntry(row, row - 1, -aw);
+			Matrix.addEntry(row, row + Nx2, -an);
+			Matrix.addEntry(row, row - Nx2, -as);
+		}
+	}
+
+	// extraneous nodes
+
+	//bottom left 
+	Matrix.addEntry(0,0,1);
+	
+	//bottom right
+	Matrix.addEntry(Nx2 - 1, Nx2 - 1, 1);
+	
+	//top left
+	Matrix.addEntry(Nx2*(Ny + 1), Nx2*(Ny + 1), 1);
+
+	//top right 
+	Matrix.addEntry((Nx2*Ny2 - 1), (Nx2*Ny2 - 1), 1);
+
+}
+
+void BuildRHS(matrixType& Matrix, 
+	      const meshType& Mesh,
+	      const caseType& Case)
+{
+	int Nx = Matrix.Nx;
+	int Ny = Matrix.Ny;
+	int Nx2 = Matrix.Nx2;
+	int My2 = Matrix.Ny2;
+
+	double dx = Mesh.dx;
+	double dy = Mesh.dy;
+	double Lx = Mesh.Lx;
+	double Ly = Mesh.Ly;
+
+	double k = Case.k;
+
+	// south/north BC (Dirichlet)
+	for (int i = 1; i <= Nx; i++)
+	{
+		//south 
+		double gs = std::cos(Mesh.xc[i]);
+		Matrix.setRHS(i, 0, gs);
+	
+		//north 
+		double gn = std::cos(Mesh.xc[i])*std::cos(Ly);
+		Matrix.setRHS(i, Ny + 1, gn);
+	}
+
+	// east/west BC (Neumann)
+	for (int j = 1; j <= Ny; j++)
+	{
+		//west 
+		double gw = -std::sin(0);
+		Matrix.setRHS(0, j, gw);
+
+		//east
+		double ge = -std::sin(Lx)*std::cos(Mesh.yc[j]);
+		Matrix.setRHS((Nx + 1), j, ge);
+	}
+
+	
+	// internal nodes
+	for (int i = 1; i <= Nx; i++)
+	{
+		for (int j = 1; j <= Ny; j++)
+		{
+			double f = 2*std::cos(Mesh.xc[i])*std::cos(Mesh.yc[j]);
+			Matrix.setRHS(i,j,dx*dy*k*f);
+		}
+	}
+}
+
+
+	
+	
+
