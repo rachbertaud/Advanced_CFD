5f9c62e8c3cbdffbf03bab10d7577947718d787b
rachelbertaud <bertaud@Alma.local>
2026-04-27 21:45:55 -0600

    Merging after deleting stuff

--- HW6/Main/TeSubmit.sh
+++ HW6/Main/TeSubmit.sh
@@ -5,8 +5,8 @@ echo 'dt, error' > t_convergence.csv
 sed -i '' "8s/.*/6.28/" T.inp	    
 
 
-sed -i '' "2s/^[0-9]*/32/" Mesh.inp  # Nx on line 2
-sed -i '' "4s/^[0-9]*/32/" Mesh.inp  # Nx on line 2
+sed -i '' "2s/^[0-9]*/64/" Mesh.inp  # Nx on line 2
+sed -i '' "4s/^[0-9]*/64/" Mesh.inp  # Nx on line 2
 
 
 for dt in 0.1 0.05 0.01 0.005 0.001 0.0005; do
